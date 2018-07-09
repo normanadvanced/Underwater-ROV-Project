c9a9e5089ca0939c5a599d4b563674bf6fab2eb3
pranavjayachand <nuancenet@gmail.com>
2018-07-09 14:19:56 -0500

    Fixed start script.

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo ds4drv &
 python3 Computer/baseServer.py &
-ssh pi@raspberrypi.local "bash Desktop/Underwater-ROV-Project/piStart.sh" &
+sshpass -p "raspberry" ssh -o StrictHostKeyChecking=no pi@raspberrypi.local "cd Desktop/Underwater-ROV-Project/ && bash piStart.sh"
 vncviewer raspberrypi.local
