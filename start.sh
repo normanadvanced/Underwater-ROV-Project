5e51ba036d6b9a417455305557c1a5b32f87ae9d
RezaTorbati <kofcrotmgiv@gmail.com>
2018-07-10 15:53:13 -0500

    Deleted the ssh part of the start.sh script

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo ds4drv &
 python3 Computer/baseServer.py &
-sshpass -p "raspberry" ssh -o StrictHostKeyChecking=no pi@raspberrypi.local "cd Desktop/Underwater-ROV-Project/ && bash piStart.sh"
 vncviewer raspberrypi.local
