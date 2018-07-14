930a7cff48490cdd1ddc5d6ea12d36205268aa64
pranavjayachand <nuancenet@gmail.com>
2018-07-13 22:24:25 -0500

    Separated base and horn servers and add more cleanup code.

---
+++ Computer/hornServer.py
@@ -0,0 +1,39 @@
+from socket import *
+from codecs import *
+import os
+import pygame
+import time
+import sys
+import serial
+import threading
+
+horn = serial.Serial('/dev/ttyACM0', 115200, timeout=.1)  # COM4 for windows, /dev/ttyAMC0
+
+#we can clear the que with pygame.event.clear()
+#pygame.event.wait() stops the program until an event becomes available
+
+
+#stuff needed for the server
+HOST = 'NARROVCommandModule.local'
+PORT = 5006
+ADDRESS = (HOST, PORT)
+server = socket(AF_INET, SOCK_STREAM)
+server.bind(ADDRESS)
+server.listen(1)
+
+
+print("Waiting for connection horn . . .")
+client, address = server.accept()
+print("horn connected from: ", address)
+
+
+#pygame.event.setBlocked
+
+
+while True:
+    if str(horn.readline())[2:-5] == 'Honk':
+        honk_time = "Honk time: " + str(time.time())
+        client.send(bytes(honk_time, "ascii"))
+
+
+
