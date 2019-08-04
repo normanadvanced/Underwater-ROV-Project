fb013b6c74e02f77097bb794a9fa79767cf3a66b
pranavjayachand <nuancenet@gmail.com>
2019-08-04 15:11:22 -0500

    Removed hydrophone.

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ bash cleanup.sh &
 vncviewer raspberrypi.local &
 sleep 4s
 echo "$(<../password.txt)" | sudo -S python3 Computer/baseServer.py &
-echo "$(<../password.txt)" | sudo -S python3 Computer/hornServer.py &
+#echo "$(<../password.txt)" | sudo -S python3 Computer/hornServer.py &
 sleep 5s
 sshpass -p "raspberry" ssh -X -o "StrictHostKeyChecking=no" pi@raspberrypi.local "cd ~/Desktop/Underwater-ROV-Project/Pi && echo raspberry | sudo -S python3 ControllerClient.py &" &
 sshpass -p "raspberry" ssh -X -o "StrictHostKeyChecking=no" pi@raspberrypi.local "cd ~/Desktop/Underwater-ROV-Project/Pi && echo raspberry | sudo -S python3 HornClient.py &" &
