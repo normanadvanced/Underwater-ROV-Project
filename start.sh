3fe541b51342e20ad41caaf886b07b84d409166d
pranavjayachand <nuancenet@gmail.com>
2019-08-09 14:33:12 -0500

    Start stuff

--- start.sh
+++ start.sh
@@ -2,9 +2,11 @@
 bash cleanup.sh &
 vncviewer raspberrypi.local &
 sleep 4s
-echo "$(<../password.txt)" | sudo -S python3 Computer/baseServer.py &
+echo "$(<../password.txt)" | sudo -S python3 Computer/baseServer.py & 
 #echo "$(<../password.txt)" | sudo -S python3 Computer/hornServer.py &
 sleep 5s
+
+sshpass -p "raspberry" ssh -X -o "StrictHostKeyChecking=no" pi@raspberrypi.local "export DISPLAY=:0 && xhost + &" &
 sshpass -p "raspberry" ssh -X -o "StrictHostKeyChecking=no" pi@raspberrypi.local "cd ~/Underwater-ROV-Project/Pi && echo raspberry | sudo -S python3 ControllerClient.py &" &
 #sshpass -p "raspberry" ssh -X -o "StrictHostKeyChecking=no" pi@raspberrypi.local "cd ~/Desktop/Underwater-ROV-Project/Pi && echo raspberry | sudo -S python3 HornClient.py &" &
 sleep 5s
